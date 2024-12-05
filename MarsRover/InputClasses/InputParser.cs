f0d4c81741d5d54be3832d6201fb53c477726aec
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-05 11:23:38 +0100

    Create InputParser class with method for parsing char input into Instruction enum

---
+++ MarsRover/InputClasses/InputParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.InputClasses
+{
+    internal static class InputParser
+    {
+        public static Instruction ParseInstruction(char input) => input switch
+        {
+            'L' => Instruction.LEFT,
+            'R' => Instruction.RIGHT,
+            'M' => Instruction.MOVE,
+            _ => throw new ArgumentException("Invalid direction input"),
+        };
+    }
+}
