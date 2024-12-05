73ff6b9778bd5aa8911b5027d71934b947ad1c0a
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-05 12:37:13 +0100

    Add Plateau and Rover classes

---
+++ MarsRover/Plateau.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    internal class Plateau
+    {
+        public PlateauSize Size { get; }
+        public List<Rover> Rovers { get; set; }
+    }
+}
---
+++ MarsRover/Rover.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    internal class Rover
+    {
+        public int ID { get; }
+        public Position Position { get; private set; }
+        public Plateau? Plateau { get; set; }
+
+        private static int RoverCount = 0;
+    }
+}
