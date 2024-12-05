098d81bc3ecce812309cd50837cc40ca4ddbe023
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-05 12:49:34 +0100

    Flesh out Plateau and Rover classes

--- MarsRover/Plateau.cs
+++ MarsRover/Plateau.cs
@@ -9,6 +9,20 @@ namespace MarsRover
     internal class Plateau
     {
         public PlateauSize Size { get; }
-        public List<Rover> Rovers { get; set; }
+        public List<Rover> Rovers { get; set; } = new List<Rover>();
+        public string Name { get; set; }
+
+
+        public Plateau(PlateauSize size, string name)
+        {
+            Size = size;
+            Name = name;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name} Plateau.";
+        }
     }
 }
--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -9,9 +9,30 @@ namespace MarsRover
     internal class Rover
     {
         public int ID { get; }
-        public Position Position { get; private set; }
+        public Position? Position { get; private set; }
         public Plateau? Plateau { get; set; }
 
         private static int RoverCount = 0;
+
+        public Rover(Position startingPosition)
+        {
+            Position = startingPosition;
+            RoverCount++;
+            ID = RoverCount;
+        }
+
+        public override string ToString()
+        {
+            string IDstring = $"Rover {ID}";
+
+            if (Position is null || Plateau is null)
+            {
+                return IDstring;
+            }
+            else
+            {
+                return $"{IDstring}, currently at {Position.ToString()} in {Plateau.ToString()}";
+            }
+        }
     }
 }
