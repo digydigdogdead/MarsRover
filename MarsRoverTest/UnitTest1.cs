1609f878d9feb4f60825e1212ebc6859f4ce92c2
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-05 11:01:00 +0100

    Add project files.

---
+++ MarsRoverTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MarsRoverTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
