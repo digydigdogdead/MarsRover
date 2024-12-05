1bc090dbe493e0d960db41edb813fbf79be78dba
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-05 11:26:44 +0100

    Add ParseDirections method to parse directions from char

--- MarsRover/InputClasses/InputParser.cs
+++ MarsRover/InputClasses/InputParser.cs
@@ -13,7 +13,16 @@ namespace MarsRover.InputClasses
             'L' => Instruction.LEFT,
             'R' => Instruction.RIGHT,
             'M' => Instruction.MOVE,
-            _ => throw new ArgumentException("Invalid direction input"),
+            _ => throw new ArgumentException("Invalid instruction input"),
+        };
+
+        public static Directions ParseDirections(char input) => input switch
+        {
+            'N' => Directions.NORTH,
+            'E' => Directions.EAST,
+            'S' => Directions.SOUTH,
+            'W' => Directions.WEST,
+            _ => throw new ArgumentException("Invalid direction input")
         };
     }
 }
