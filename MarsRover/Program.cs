98f7626b8ade3440cec60f17243867a392eecfad
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-06 10:26:05 +0100

    Add UI class with Welcome, DiscoverPlateau, and BuildRover methods

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,26 +1,28 @@
 ﻿using MarsRover;
 using MarsRover.InputClasses;
+using MarsRover.UI;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        PlateauSize testSize = Creator.CreatePlateauSize("5 5");
-        Position testPosition = Creator.CreatePosition("1 2 N");
-        Plateau plateau = new Plateau(testSize, "The Red Wastes");
-        Rover rover1 = new Rover();
+        /*        PlateauSize testSize = Creator.CreatePlateauSize("5 5");
+                Position testPosition = Creator.CreatePosition("1 2 N");
+                Plateau plateau = new Plateau(testSize, "The Red Wastes");
+                Rover rover1 = new Rover();
 
-        MissionControl.DeployRover(rover1, plateau, testPosition);
+                MissionControl.DeployRover(rover1, plateau, testPosition);
 
-        Console.WriteLine(rover1.Position.ToString());
+                Console.WriteLine(rover1.Position.ToString());
 
-        rover1.ReceiveInstructions("RMMLMM");
+                rover1.ReceiveInstructions("RMMLMM");
 
-        Console.WriteLine(rover1.Position.ToString());
+                Console.WriteLine(rover1.Position.ToString());
 
-        rover1.ReceiveInstructions("MMMMMMMMMMMM");
+                rover1.ReceiveInstructions("MMMMMMMMMMMM");
 
-        Console.WriteLine(rover1.Position.ToString());
+                Console.WriteLine(rover1.Position.ToString());*/
 
+        UI.Welcome();
     }
 }
---
+++ MarsRover/UI/UI.cs
@@ -0,0 +1,78 @@
+﻿using MarsRover.InputClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.UI
+{
+    internal static class UI
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to Mission Control!");
+            DiscoverPlateau();
+            Console.WriteLine();
+            BuildRover();
+
+        }
+
+        public static void DiscoverPlateau()
+        {
+            Console.WriteLine("We have discovered a new plateau on Mars that needs analysing. What would you like to name it?");
+            bool isNameNull = true;
+            string? nameInput = "";
+            while (isNameNull)
+            {
+                nameInput = Console.ReadLine();
+                if (String.IsNullOrEmpty(nameInput))
+                {
+                    Console.WriteLine("This plateau needs a name!");
+                }
+                else isNameNull = false;
+            }
+            Console.WriteLine("I've left my space ruler at home. How big do you reckon it is?");
+            bool sizeCreated = false;
+            while (!sizeCreated)
+            {
+                Console.WriteLine("Input a size in the format \"x y\"");
+                string? sizeInput = Console.ReadLine();
+                try
+                {
+                    PlateauSize newSize = Creator.CreatePlateauSize(sizeInput);
+                    Plateau newPlateau = new Plateau(newSize, nameInput);
+                    Console.WriteLine($"{newPlateau.Name} has been added to the database.");
+                    sizeCreated = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static void BuildRover()
+        {
+            Console.WriteLine("Building new rover...");
+            Rover newRover = new Rover();
+            Console.WriteLine($"New Rover built! This Rover's ID is {newRover.ID}");
+            Console.WriteLine("Would you like to deploy this Rover immediately? Input Y or N.");
+            bool yOrNValid = false;
+            bool userChoice = false;
+            while (!yOrNValid)
+            {
+                string? yOrNInput = Console.ReadLine();
+                yOrNValid = InputParser.TryGetYesOrNo(yOrNInput, out userChoice);
+                if (!yOrNValid) Console.WriteLine("Input was invalid, please try again.");
+            }
+
+
+        }
+
+        public static void GetAllPlateaus()
+        {
+           
+        }
+    }
+}
