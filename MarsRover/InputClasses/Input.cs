18e58e87c47c1bd4c2e722fa7100e2617227a9d0
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-06 13:22:06 +0100

    Add ChooseRoverAtSource method and GetAllRoversAtSource method

--- MarsRover/InputClasses/Input.cs
+++ MarsRover/InputClasses/Input.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MarsRover;
+using MarsRover.UI;
 
 namespace MarsRover.InputClasses
 {
@@ -40,6 +41,46 @@ namespace MarsRover.InputClasses
             }
 
             return MissionControl.Plateaus[number - 1];
+        }
+
+        public static Rover? ChooseRoverAtSource(List<Rover> source)
+        {
+            if (source.Count == 0)
+            {
+                Console.WriteLine("No available Rovers.");
+                return null;
+            }
+            
+            Console.WriteLine("Available rovers are:");
+            UI.UI.GetAllRoversAtSource(source);
+            Console.WriteLine("Please input the number of your choice or type \"cancel\" to Cancel:");
+            string? userInput = Console.ReadLine();
+            if (String.IsNullOrEmpty(userInput))
+            {
+                Console.WriteLine("You must input a number or cancel.");
+                return ChooseRoverAtSource(source);
+            }
+
+            if (userInput.ToLower() == "cancel")
+            {
+                return null;
+            }
+
+            bool isNumber = Int32.TryParse(userInput, out var number);
+
+            if (!isNumber)
+            {
+                Console.WriteLine("Input was not a number. Please try again.");
+                return ChooseRoverAtSource(source);
+            }
+
+            if (number > source.Count || number < 1)
+            {
+                Console.WriteLine("That Rover does not exist. Please try again.");
+                return ChooseRoverAtSource(source);
+            }
+
+            return source[number - 1];
         }
     }
 }
